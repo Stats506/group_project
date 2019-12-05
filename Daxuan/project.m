066591eb973137e6fa381d4958fd73988df3b74b
daxuand <55365545+daxuand@users.noreply.github.com>
2019-12-05 00:42:00 -0500

    Third draft
    Use residual bootstrap

--- Daxuan/project.m
+++ Daxuan/project.m
@@ -36,7 +36,8 @@ dt.yr = (dt.yr - mean(dt.yr)) ./ std(dt.yr);
 dt.edu = categorical(dt.edu);
 dt.pir = (dt.pir - mean(dt.pir)) ./ std(dt.pir);
 dt.health = categorical(dt.health);
-dt.health = reordercats(dt.health, {'3','2','1'});
+% set 'Good' as base level
+dt.health = reordercats(dt.health, {'3','1','2'});
 
 % run OLS
 md = fitlm(dt, 'alcohol ~ sex + yr + edu + pir + health');
@@ -44,102 +45,41 @@ md = fitlm(dt, 'alcohol ~ sex + yr + edu + pir + health');
 % extract fitted values and residuals 
 fit = predict(md, dt(:, 3:7));
 res = md.Residuals.Raw;
-
-% calculate observed coverage probability
-df = height(dt) - width(dt) -1;
-se = sqrt(sum(res .^2) / df);
-ci = [fit - tinv(0.95, df) * se, fit + tinv(0.95, df) * se];
-cover = sum(dt.alcohol > ci(:,1) & dt.alcohol < ci(:,2)) / length(ci);
+coef = md.Coefficients(:,1);
 
 % plot residuals
-plot(1:length(ci), res, 'x'), title('residuals of OLS')
+plot(1:height(dt), res, 'x'), title('residuals of OLS')
 
 % bootstrap
 rng(506);
 nboot = 1000;
 
-func_se = @(x)sqrt(sum(x .^2) / df);
-se_boot = bootstrp(nboot, func_se, res);
-cover_boot = zeros(nboot, 1);
+% resample residuals
+func = @(x)x;
+res_boot = bootstrp(nboot, func, res);
+
+dt_boot = dt(:, 3:7);
+beta_boot = zeros(nboot, 10);
 
 for i=1:nboot
-    ci_boot = [fit - tinv(0.95, df) * se_boot(i), ...
-        fit + tinv(0.95, df) * se_boot(i)];
-    cover_boot(i) = sum(dt.alcohol > ci_boot(:,1) & ...
-        dt.alcohol < ci_boot(:,2)) / length(ci);
-end
-
-% consider 0, 10, ... , 100 quantile, mean and the original observed value
-% as estimate candidates
-se_list = ([quantile(se_boot, (0:10) / 10), mean(se_boot), se])' ;
-cover_list = zeros(13, 1);
-
-for i=1:13
-    ci_list = [fit - tinv(0.95, df) * se_list(i), ...
-        fit + tinv(0.95, df) * se_list(i)];
-    cover_list(i) = sum(dt.alcohol > ci_list(:,1) & ...
-        dt.alcohol < ci_list(:,2)) / length(ci);
-end
-
-ratio_list = cover_list ./ se_list;
-
-% plot se
-subplot(2,2,1)
-plot(linspace(0,100,11), se_list(1:11), 'rx', ...
-     110, se_list(12), 'bx', ...
-     linspace(0,120,13), se_list(13) * ones(13,1), 'k--'), ...
-     title('std. err'), ...
-     legend('quantile', 'mean', 'observed value', 'Location','southeast')
- 
-% plot coverage (probability)
-subplot(2,2,2)
-plot(linspace(0,100,11), cover_list(1:11), 'rx', ...
-     110, cover_list(12), 'bx', ...
-     linspace(0,120,13), cover_list(13) * ones(13,1), 'k--'), ...
-     title('cover'), ...
-     legend('quantile', 'mean', 'observed value', 'Location','southeast')
- 
-% plot cover / se
-subplot(2,2,[3,4])
-plot(linspace(0,100,11), ratio_list(1:11), 'rx', ...
-     110, ratio_list(12), 'bx', ...
-     linspace(0,120,13), ratio_list(13) * ones(13,1), 'k--'), ...
-     title('cover / se'), ...
-     legend('quantile', 'mean', 'observed value', 'Location','northeast')
- 
-%test
-nrep = 1000;
-
-count = 0;
-
-for i=1:nrep
-    se_boot = bootstrp(nboot, func_se, res);
-    cover_boot = zeros(nboot, 1);
-
-    for j=1:nboot
-        ci_test = [fit - tinv(0.95, df) * se_boot(j), ...
-            fit + tinv(0.95, df) * se_boot(j)];
-        cover_boot(j) = sum(dt.alcohol > ci_test(:,1) & ...
-            dt.alcohol < ci_test(:,2)) / length(ci);
-    end
-
-    se_list = ([quantile(se_boot, (0:10) / 10), mean(se_boot), se])' ;
-    cover_list = zeros(13, 1);
-
-    for j=1:13
-        ci_list = [fit - tinv(0.95, df) * se_list(j), ...
-        fit + tinv(0.95, df) * se_list(j)];
-        cover_list(j) = sum(dt.alcohol > ci_list(:,1) & ...
-        dt.alcohol < ci_list(:,2)) / length(ci);
-    end
+    % generate new response
+    dt_boot.alcohol = fit + res_boot(i,:)';
     
-    ratio_list = cover_list ./ se_list;
+    % fit new model
+    md_boot = fitlm(dt_boot, 'alcohol ~ sex + yr + edu + pir + health');
     
-    if( max(ratio_list) == ratio_list(1) ) 
-        count = count +1;
-    end
+    % extract new estimate
+    beta_boot(i,:) = table2array(md_boot.Coefficients(:,1))';
 end
 
-prob = count / nrep;
+% calculate std err
+se = std(beta_boot);
 
-prob
+% summary
+result = coef;
+result.se = se';
+result.t = result.Estimate ./ result.se;
+result.pvalue = 1-tcdf(abs(result.t),1);
+
+result
+
